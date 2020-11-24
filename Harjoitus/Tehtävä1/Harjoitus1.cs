411f6ee83fc4b458f1fe90857e0c9b7fa2d30816
timi.wahlman@edu.keuda.fi <timi.wahlman@edu.keuda.fi>
2020-11-24 10:48:06 +0200

    Add project files.

---
+++ Harjoitus/Tehtävä1/Harjoitus1.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //laskeYhteen(3, 5);
+            Tehtava2(10, 35);
+
+        }
+        public static void laskeYhteen(int luku1, int luku2)
+        {
+            Console.WriteLine(luku1 + luku2);
+        }
+
+        public static void Tehtava2(int luku1, int luku2)
+        {
+            laske(luku1, luku2);
+        }
+        public static void laske(int luku1, int luku2)
+        {
+            Console.WriteLine((luku1 + luku2) * 1.8);
+        }
+    }
+}
